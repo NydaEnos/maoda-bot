545989fb2b79abaae19a35ed39b5518dfd7e4fb5
NydaEnos <sholva91@gmail.com>
2022-12-30 21:16:36 +0100

    restore old version

--- index.js
+++ index.js
@@ -72,66 +72,34 @@ client.on("messageCreate", async (message) => {
     
 
     else if (command === "new") {
-
-        if (!args.length) {
-            booru.posts({ tags: 'damao_yu' }).then(async (posts) => {
-                // Select last image
-                const post = posts[0]
-                if (lastIdPost == post.id) {
-                    await message.channel.send({content: "No new image from Maoda-sama :cry:" })
-                }
-                else {
-                    const url = booru.url(post.file_url)
-                    const attachment = new MessageAttachment(url.href)
-                    let res = await axios.get(url.href)
-                    let char_name = post.tag_string_character
-                    const contentLength = res.headers['content-length']
+        booru.posts({ tags: 'damao_yu' }).then(async (posts) => {
+            // Select last image
+            const post = posts[0]
+            if (lastIdPost == post.id) {
+                await message.channel.send({content: "No new image from Maoda-sama :cry:" })
+            }
+            else {
+                const url = booru.url(post.file_url)
+                const attachment = new MessageAttachment(url.href)
+                let res = await axios.get(url.href)
+                let char_name = post.tag_string_character
+                const contentLength = res.headers['content-length']
+            
+                let fileInMb = parseFloat(contentLength) / (1024 ** 2)
                 
-                    let fileInMb = parseFloat(contentLength) / (1024 ** 2)
-                    
-                    // Send content + image file if file < 8Mb
-                    if (fileInMb > 8) {
-                        await message.channel.send({content: `Maoda-sama last image : \n${char_name} \n${url.href} \n(file is too big)`});
-        
-                    }
-        
-                    else {
-                        await message.channel.send({content: `Maoda-sama last image : \n${char_name}`, files: [attachment] });
-                    }
-                    lastIdPost = post.id
+                // Send content + image file if file < 8Mb
+                if (fileInMb > 8) {
+                    await message.channel.send({content: `Maoda-sama last image : \n${char_name} \n${url.href} \n(file is too big)`});
+    
+                }
+    
+                else {
+                    await message.channel.send({content: `Maoda-sama last image : \n${char_name}`, files: [attachment] });
+                }
+                lastIdPost = post.id
                 }
             })
         }
-        else {
-            const usrInput = parseInt(args[0])
-            console.log(`User has selected ${args[0]} images.`)
-            for (let i = 0; i < usrInput; i++) {
-                booru.posts({ tags: 'damao_yu' }).then(async (posts) => {
-                    
-                        const post = posts[i]
-                        const url = booru.url(post.file_url)
-                        const attachment = new MessageAttachment(url.href)
-                        let res = await axios.get(url.href)
-                        let char_name = post.tag_string_character
-                        const contentLength = res.headers['content-length']
-                    
-                        let fileInMb = parseFloat(contentLength) / (1024 ** 2)
-                        
-                        // Send content + image file if file < 8Mb
-                        if (fileInMb > 8) {
-                            await message.channel.send({content: `Maoda-sama last image : \n${char_name} \n${url.href} \n(file is too big)`});
-            
-                        }
-            
-                        else {
-                            await message.channel.send({content: `Maoda-sama last image : \n${char_name}`, files: [attachment] });
-                        }
-                    
-                })
-            }
-        }
-    }
-
 
     else if (command === "help") {
         message.channel.send({content: `All available commands : \n!!new : check if there is a new image from Maoda-sama \n!!help : get all commands \n!!random : return a random image from Maoda-sama \n!!last : return last image from Maoda-sama \n!!search : search for a specific image of Maoda-sama (WIP)` });
